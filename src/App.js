61e9cd584d05630674a6258f3cc7bc5561e33609
Ankit <ak.kumar_1996@rediffmail.com>
2023-08-24 11:23:04 +0530

    signup page completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@
 import './App.css';
 import LoginPage from './components/LoginPage';
 import SharePostPage from './components/SharePostPage';
+import SignUpPage from './components/SignUpPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      {/* <LoginPage /> */}
-      <SharePostPage />
+      <Routes>
+      <Route path = "/" element ={<LoginPage />} />
+      <Route path = "/dashboard" element={<SharePostPage />} />
+      <Route path = "/signup" element={<SignUpPage />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/SignUpPage.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+
+function SignUpPage() {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const history = useNavigate();
+
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Email',email);
+    console.log('Password',password);
+  };
+
+  const handleContinue = () => {
+    history('/dashboard');
+  };
+
+
+  return (
+    <div className='signup-page'>
+      <div className='signup-box'>
+        <h1>Sign Up</h1>
+        <h1>Create an Account to continue</h1>
+        <div className='email-input-box'>
+          <label htmlFor='email'>Email</label>
+          <div className='email-input'>
+          <input
+            type='email'
+            id='email'
+            placeholder='Enter your email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          </div>
+        </div>
+        <div className='username-input-box'>
+          <label htmlFor='username'>Username</label>
+          <div className='username-input'>
+          <input
+            type='text'
+            id='username'
+            placeholder='Choose your preferred username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          </div>
+        </div>
+        <div className='password-input-box'>
+          <label htmlFor='password'>Password</label>
+          <div className='password-input'>
+            <span>
+            <FontAwesomeIcon
+              icon={showPassword ? faEyeSlash : faEye}
+              className='eye-icon'
+              onClick={handlePasswordToggle}
+            />
+            </span>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              id='password'
+              placeholder='Choose a strong password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+           
+          </div>
+        </div>
+        <button onClick={handleContinue} className='signup-btn'>Continue</button>
+        <div className='login-option'>
+            <span>Already have an account? 
+                <a href="/">Login 
+                <FontAwesomeIcon icon={faArrowRight} />
+                </a></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUpPage;
